6994fdd6977fc6b1424e8b2395bd47125c04e3da
Benoît Hubert <benoithubert@gmail.com>
2018-11-26 15:38:16 +0100

    Exemple utilisation Promise avec async/await

--- promisify/readFile.js
+++ promisify/readFile.js
@@ -3,8 +3,17 @@ const { promisify } = require('util');
 
 fs.readFileAsync = promisify(fs.readFile.bind(fs));
 
-fs.readFileAsync('package.json')
+// fs.readFileAsync('package.json')
+//   .then(buf => buf.toString())
+//   .then(JSON.parse)
+//   .then(content => console.log(content))
+//   .catch(err => console.error(err));
+
+const readJSON = async file => fs.readFileAsync(file)
   .then(buf => buf.toString())
-  .then(JSON.parse)
-  .then(content => console.log(content))
-  .catch(err => console.error(err))
+  .then(JSON.parse);
+
+(async () => {
+  const pkgContent = await readJSON('package.json');
+  console.log(pkgContent);
+})();
